247dac5b146767e7ec61179296cc3ee9441c149a
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-11-08 10:46:23 +0000

    Moving to FactCheck

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,18 @@
-using Base.Test
-include(joinpath(Pkg.dir("SIUnits.jl"), "src", "units.jl"))
+module TestUnits
+using FactCheck
+using Units
 
-if VERSION <= v"0.3-"
-    macro test_throws_compat(a,b)
-        esc(:(@test_throws $b))
-    end
-else
-    macro test_throws_compat(a,b)
-        esc(:(@test_throws $a $b))
-    end
+
+facts("dimensionality") do
+  @fact Length * Length --> @dimensionality length:2
+  @fact Length * Time --> @dimensionality length:1 time:1
+  @fact Length * Time --> not(@dimensionality length:2 time:1)
+  @fact Energy / Length --> @dimensionality length:1 mass:1 time:-2
+  @fact Energy / Length^2 * Time^2 / Mass --> Nondimensional
+  @fact inv(Energy) --> @dimensionality length:-2 mass:-1 time:2
 end
+# @fact conversion_factor(Units.kilometer, Units.gigameter) == 1e-6
+# @pending @test_throws Exception Units.conversion_factor(Units.kilometer, Units.gigagram)
 
-if VERSION <= v"0.4.0-dev+3338"
-    const AssertionError = ErrorException
+FactCheck.exitstatus()
 end
-
-@test is(Units.Length * Units.Length, Units.@dimensionality length:2)
-@test is(Units.Length * Units.Time, Units.@dimensionality length:1 time:1)
-@test !is(Units.Length * Units.Time, Units.@dimensionality length:2 time:1)
-@test is(Units.Energy / Units.Length, Units.@dimensionality length:1 mass:1 time:-2)
-@test is(Units.Energy / Units.Length^2 * Units.Time^2 / Units.Mass, Units.Undimensional)
-@test is(inv(Units.Energy), Units.@dimensionality length:-2 mass:-1 time:2)
-@test Units.conversion_factor(Units.kilometer, Units.gigameter) == 1e-6
-@test_throws Exception Units.conversion_factor(Units.kilometer, Units.gigagram)
